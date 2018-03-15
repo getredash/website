a0b88a25e3faf52113c055575e75f5e069d60477
Arik Fraimovich <arik@arikfr.com>
2018-03-16 00:30:28 +0200

    Make sure we show the last search results

--- website/_assets/javascript/docs_search.js
+++ website/_assets/javascript/docs_search.js
@@ -1,11 +1,17 @@
 $(function() {
+  var last_query = null;
   $('.search-box').on('keyup', function() {
     var query = $(this).val();
     var client = algoliasearch(window.ALGOLIA.applicationId, window.ALGOLIA.searchApiKey);
     var index = client.initIndex('docs');
 
     if (query) {
+      last_query = query;
       index.search(query, function(err, content) {
+        if (content.query !== last_query) {
+          // we made another query already, so no need to show this one.
+          return;
+        }
         var resultsHtml = '<ul class="topics__list list-unstyled">';
         content.hits.forEach(function(hit) {
           var result = '<li class="topics__list__item"><a href="{{article.url}}" class="topics__list__link"><h4 class="topics__list__title">{{article.title}}</h4><p>{{article.description}}</p></a></li>';
