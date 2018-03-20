8716299f84a8b57ed5073e331700fc38e8099729
Arik Fraimovich <arik@arikfr.com>
2018-03-20 14:57:50 +0200

    Contributors data file

---
+++ bin/fetch-contributors.js
@@ -0,0 +1,51 @@
+const request = require('request');
+const fs = require('fs');
+
+function getPage(page) {
+	const url = "https://api.github.com/repos/getredash/redash/contributors";
+	const qs = {
+		per_page: 100,
+		page
+	};
+	const headers = {
+		'User-Agent': 'Contributors Downloader'
+	}
+
+	return new Promise((resolve, reject) => {
+		request(url, {qs, headers}, function(error, response, body) {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(JSON.parse(body));
+			}
+		})
+	})
+}
+
+async function getContributors() {
+	let hasMore = true;
+	let page = 1;
+	let contributors = [];
+
+	while (hasMore) {
+		const currentPage = await getPage(page);
+
+		if (currentPage.length === 0) {
+			hasMore = false;
+		} else {
+			contributors = contributors.concat(currentPage);
+		}
+
+		page +=1;
+	}
+
+	return contributors;
+}
+
+async function printContributors() {
+	const contributors = await getContributors();
+
+	console.log(JSON.stringify(contributors, null, 2));
+}
+
+printContributors();
