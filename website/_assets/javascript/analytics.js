425bea364a93a69c8143793ef3995882b83707bc
Arik Fraimovich <arik@arikfr.com>
2018-03-21 12:35:45 +0200

    Remove HotJar

--- website/_assets/javascript/analytics.js
+++ website/_assets/javascript/analytics.js
@@ -6,16 +6,7 @@ analytics.page()
 
 // Intercom
 window.intercomSettings = {
-  app_id: "rwp8rrzy"
+  app_id: "rwp8rrzy",
+  hide_default_launcher: true
 };
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rwp8rrzy';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-
-// HotJar
-(function(h,o,t,j,a,r){
-  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-  h._hjSettings={hjid:244496,hjsv:5};
-  a=o.getElementsByTagName('head')[0];
-  r=o.createElement('script');r.async=1;
-  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-  a.appendChild(r);
-})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
