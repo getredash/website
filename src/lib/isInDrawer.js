ac0b0236b7129b2c84629e7563d3de9332505550
Arik Fraimovich <arik@arikfr.com>
2019-02-13 18:26:52 +0200

    Revert "Fix server side build"
    This reverts commit 0c9df3781b3923f0760d8ba234b9b77695155f5b.

--- src/lib/isInDrawer.js
+++ src/lib/isInDrawer.js
@@ -1,9 +1,4 @@
 export default function isInDrawer() {
-  const result = false
-  // Need try/catch because in server mode this is not available.
-  try {
-    const urlParams = new URLSearchParams(window.location.search)
-    result = !!urlParams.get('drawer-mode')
-  } catch (error) {}
-  return result
+  const urlParams = new URLSearchParams(window.location.search)
+  return !!urlParams.get('drawer-mode')
 }
