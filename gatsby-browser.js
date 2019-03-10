0cd7295e68069bb4f6df76471a47c0dd4e36575d
Ran Byron <ranbena@gmail.com>
2019-03-10 09:22:48 +0200

    Removed legacy scrollTo code (#220)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,3 @@
 exports.onRouteUpdate = ({ location }) => {
-  window.analytics && window.analytics.page();
-  if (
-    typeof window !== 'undefined' &&
-    (!location.action || location.action === 'push')
-  ) {
-    setTimeout(() => {
-      window.scrollTo(0, 0)
-    })
-  }
+  window.analytics && window.analytics.page()
 }
