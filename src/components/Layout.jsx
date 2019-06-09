09d1e7781950df310b339b8f354220b5d6ebd304
Ran Byron <ranbena@gmail.com>
2019-06-09 12:12:25 +0300

    Reverted to functional component with useEffect

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
@@ -14,78 +14,72 @@ function reportUrlChange() {
   window.parent.postMessage(data, '*')
 }
 
-export default class extends React.Component {
-  componentDidMount() {
+export default ({ children, title, description, location }) => {
+  useEffect(() => {
     window.addEventListener('hashchange', reportUrlChange)
     reportUrlChange()
-  }
+    return () => window.removeEventListener('hashchange', reportUrlChange)
+  }, [])
 
-  componentWillUnmount() {
-    window.removeEventListener('hashchange', reportUrlChange)
-  }
+  return (
+    <div className="wrapper">
+      <Helmet>
+        <title>{title || 'Make Your Company Data Driven'}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+        <meta
+          name="keywords"
+          content="Query, SQL, Visualize, Data, Insights, Dashboard, Alert, Alerts, web based sql client, integration, integrations, Amazong Redshift, BigQuery, PotsgreSQL, MySQL, TreasureData, MS SQL Server, MongoDB, ElasticSearch, Hive, Google Spreadsheets, Impala, InfluxDB, Presto, ScyllaDB, Cassandra, Amazon DynamoDB, Python, Vertica, Graphite, Stitch, URL"
+        />
+        <link rel="icon" href="/assets/images/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/assets/images/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/images/favicon-16x16.png"
+        />
 
-  render() {
-    const { children, title, description, location } = this.props
-    return (
-      <div className="wrapper">
-        <Helmet>
-          <title>{title || 'Make Your Company Data Driven'}</title>
-          <meta name="title" content={title} />
-          <meta name="description" content={description} />
-          <meta
-            name="keywords"
-            content="Query, SQL, Visualize, Data, Insights, Dashboard, Alert, Alerts, web based sql client, integration, integrations, Amazong Redshift, BigQuery, PotsgreSQL, MySQL, TreasureData, MS SQL Server, MongoDB, ElasticSearch, Hive, Google Spreadsheets, Impala, InfluxDB, Presto, ScyllaDB, Cassandra, Amazon DynamoDB, Python, Vertica, Graphite, Stitch, URL"
-          />
-          <link rel="icon" href="/assets/images/favicon.ico" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/assets/images/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="96x96"
-            href="/assets/images/favicon-96x96.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/assets/images/favicon-16x16.png"
-          />
+        <meta name="format-detection" content="telephone=no" />
 
-          <meta name="format-detection" content="telephone=no" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:url"
+          content={`https://redash.io${location && location.pathname}`}
+        />
+        <meta
+          property="og:image"
+          content="https://redash.io/assets/images/social-facebook-1200x630.png"
+        />
 
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          <meta
-            property="og:url"
-            content={`https://redash.io${location && location.pathname}`}
-          />
-          <meta
-            property="og:image"
-            content="https://redash.io/assets/images/social-facebook-1200x630.png"
-          />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@getredash" />
+        <meta name="twitter:creator" content="@getredash" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta
+          name="twitter:image"
+          content="https://redash.io/assets/images/social-twitter-600x314.png"
+        />
 
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@getredash" />
-          <meta name="twitter:creator" content="@getredash" />
-          <meta name="twitter:title" content={title} />
-          <meta name="twitter:description" content={description} />
-          <meta
-            name="twitter:image"
-            content="https://redash.io/assets/images/social-twitter-600x314.png"
-          />
-
-          <script src="/assets/scripts/a.js" />
-        </Helmet>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </div>
-    )
-  }
+        <script src="/assets/scripts/a.js" />
+      </Helmet>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  )
 }
