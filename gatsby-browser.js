84966e584dacfae711c4dcfac5a49768875c1cdf
Ran Byron <ranbena@getredash.com>
2019-06-09 20:40:05 +0300

    Report url change to parent (#252)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,10 @@
+const { reportUrlChange, RESET_URL } = require('components/Layout')
+
 exports.onRouteUpdate = ({ location }) => {
   window.analytics && window.analytics.page()
+}
+
+exports.onInitialClientRender = () => {
+  // when leaving website, reset url
+  window.onbeforeunload = () => reportUrlChange(RESET_URL)
 }
