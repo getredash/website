c28bd770ac0ceb85cf7f31f1b96508b5de51e3f8
SeongTae Jeong <seongtaejg@gmail.com>
2024-04-12 02:52:03 +0000

    Remove 'mergeLinkHeaders' option due to deprecated

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,6 @@ const config = {
       resolve: 'gatsby-plugin-netlify',
       options: {
         mergeSecurityHeaders: false,
-        mergeLinkHeaders: true,
         mergeCachingHeaders: true,
       },
     },
